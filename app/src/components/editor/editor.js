9775654fb5e927f13f9cd638b386e83260045301
Kirill <shabkir02@gmail.com>
2020-11-01 00:46:43 +0600

    ninth commit

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -9,6 +9,7 @@ import ConfirmModal from '../confirm-modal';
 import ChooseModal from '../choose-modal';
 import Panel from '../panel';
 import EditorMeta from '../editor-meta';
+import EditorImages from '../editor-images';
 
 export default class Editor extends Component {
     constructor() {
@@ -50,6 +51,7 @@ export default class Editor extends Component {
             .get(`../${page}?rnd=${Math.random()}`)
             .then(res => DOMHelper.parseStrToDOM(res.data))
             .then(DOMHelper.wrapTextNodes)
+            .then(DOMHelper.wrapImages)
             .then(dom => {
                 this.virtualDom = dom;
                 return dom;
@@ -69,6 +71,7 @@ export default class Editor extends Component {
         this.isLoading();
         const newDom = this.virtualDom.cloneNode(this.virtualDom);
         DOMHelper.unwrapTextNodes(newDom);
+        DOMHelper.unwrapImages(newDom);
         const html = DOMHelper.serializeDOMToString(newDom);
         await axios
             .post('./api/savePage.php', {pageName: this.currentPage, html})
@@ -86,6 +89,13 @@ export default class Editor extends Component {
 
             new EditorText(element, virtualElement);
         });
+
+        this.iframe.contentDocument.body.querySelectorAll('[editableimgid]').forEach(element => {
+            const id = element.getAttribute('editableimgid');
+            const virtualElement = this.virtualDom.body.querySelector(`[editableimgid="${id}"]`);
+
+            new EditorImages(element, virtualElement);
+        });
     }
 
     injectStyles() {
@@ -97,6 +107,10 @@ export default class Editor extends Component {
             }
             text-editor:focus {
                 outline: 3px solid red;
+                outline-offset: 8px;
+            }
+            [editableimgid]:hover {
+                outline: 3px solid orange;
                 outline-offset: 8px;
             }
         `;
@@ -154,6 +168,8 @@ export default class Editor extends Component {
         return (
             <>
                 <iframe src='' frameBorder="0"></iframe>
+                <input id="img-upload" type="file" accept="image/*" style={{display: 'none'}}></input>
+
                 {spinner}
 
                 {/* <button 
